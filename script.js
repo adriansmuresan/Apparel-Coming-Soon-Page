bbf06d0725a069f6f062fd0a62fc68666fa6fc27
Adrian S MUresan <adriansmuresan@gmail.com>
2020-07-09 12:08:47 -0500

    Adds the script file, sets the variables and adds a listner on the form

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const emailVal = email.value;
+
+  //check for valid email
+  
+
+})
